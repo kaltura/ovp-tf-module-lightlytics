520c627e7a00a53f78762f17d9edbff0002af561
dennis.litvak <dennis.litvak@kaltura.com>
2021-12-19 17:20:35 +0200

    start

---
+++ var.tf
@@ -0,0 +1,12 @@
+variable "environment" {}
+variable "node_env" {}
+variable "env" {}
+variable "api_url" {}
+variable "api_token" {}
+variable "batch_size" {}
+variable "memory_size" {}
+variable "timeout" {
+  default = 120
+}
+
+
