fee98604f38b8b09a772fd479f3186abd3b14f78
dennis.litvak <dennis.litvak@kaltura.com>
2021-12-26 16:59:52 +0200

    start

--- v1.0/var.tf
+++ v1.0/var.tf
@@ -120,3 +120,21 @@ variable "lambda_flow_logs_cloud_watch_architectures_lambda" {
 # ${version} ${account-id} ${action} ${bytes} ${dstaddr} ${end} ${instance-id} ${interface-id} ${log-status} ${packets} ${pkt-dstaddr} ${pkt-srcaddr} ${protocol} ${region} ${srcaddr} ${srcport} ${dstport} ${start} ${vpc-id} ${subnet-id} ${tcp-flags}
 
 
+
+
+
+resource "aws_s3_bucket" "flowLog-bucket" {
+  count = var.ShouldCollectFLowLogs ? 1 : 0
+  bucket = var.s3_flowLog
+}
+
+#############
+
+resource "aws_s3_bucket_notification" "lambda-trigger" {
+  count = var.ShouldCollectFLowLogs ? 1 : 0
+  bucket = "${aws_s3_bucket.flowLog-bucket.id}"
+lambda_function {
+  lambda_function_arn = "${aws_lambda_function.lightlytics-FlowLogs-lambda.arn}"
+  events              = ["s3:ObjectCreated:*"]
+  }
+}
