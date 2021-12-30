f183a7cfd34cdca82f6cd6824848c5249d5fb2b8
dennis.litvak <dennis.litvak@kaltura.com>
2021-12-30 11:59:51 +0200

    start

---
+++ v1.0/curl.tf
@@ -0,0 +1,18 @@
+resource "null_resource" "lightlytics-enable-account" {
+  provisioner "local-exec" {
+      depends_on = [aws_iam_role.lightlytics-role]
+      command = <<EOF
+        curl -X POST '${var.lightlytics_api_url}/graphql \
+          -H 'Content-Type: application/json' \
+          -H 'Authorization: Bearer ${var.lightlytics_auth_token}' \
+          -d '{
+                "query":"mutation AccountAcknowledge($input: AccountAckInput){\r\n accountAcknowledge(account: $input)\r\n }",
+                "variables": {
+                  "input": {
+                    "lightlytics_internal_account_id":"${var.LightlyticsInternalAccountId}",
+                    "role_arn":"arn:aws:iam::"${var.account_id}":role/"${var.env_name_prefix}-lightlytics-role"",
+                    "account_type":"AWS","account_aliases":"","aws_account_id":"${var.account_id}","stack_region":"${var.aws_region}",
+                    "stack_id":"","init_stack_version":1}}}'
+EOF
+  }
+}
