be5ea355f161e72e17358846673bd7e0df7ff2a6
dennis.litvak <dennis.litvak@kaltura.com>
2022-01-05 14:09:29 +0200

    cloud watch additional rules

--- v1.1/cloud_watch.tf
+++ v1.1/cloud_watch.tf
@@ -1,27 +1,15 @@
 locals {
+  list             = ["1","2","3","4"]
   Cloud_Watch_Rules = {
-    Lightlytics-Rule-0 = {
-      description = "Cloud Trail to Lightlytics Collection Lambda"
-      is_enabled  = true
-      event_pattern = file("${path.module}/templates/CloudWatch-Lightlytics-Rule-0.json")
-    }
-    Lightlytics-Rule-1 = {
-      description = "Cloud Trail to Lightlytics Collection Lambda"
-      is_enabled  = true
-      event_pattern = file("${path.module}/templates/CloudWatch-Lightlytics-Rule-1.json")
-    }
-    Lightlytics-Rule-2 = {
-      description = "Cloud Trail to Lightlytics Collection Lambda"
-      is_enabled  = true
-      event_pattern = file("${path.module}/templates/CloudWatch-Lightlytics-Rule-2.json")
-    }
-    Lightlytics-Rule-3 = {
-      description = "Cloud Trail to Lightlytics Collection Lambda"
-      is_enabled  = true
-      event_pattern = file("${path.module}/templates/CloudWatch-Lightlytics-Rule-3.json")
+    for_each = local.list
+    Lightlytics-Rule-[each.key] = {
+      description   = "Cloud Trail to Lightlytics Collection Lambda"
+      is_enabled    = true
+      event_pattern = file("${path.module}/templates/CloudWatch-Lightlytics-Rule-${each.key}.json")
     }
   }
 }
+
 
 resource "aws_cloudwatch_event_rule" "lightlytics-CloudWatch-rule" {
   for_each = local.Cloud_Watch_Rules
@@ -30,7 +18,6 @@ resource "aws_cloudwatch_event_rule" "lightlytics-CloudWatch-rule" {
   is_enabled = each.value["is_enabled"]
   event_pattern = each.value["event_pattern"]
 }
-
 
 
 resource "aws_cloudwatch_event_target" "lightlytics-lambda-cloud-watch-target" {
