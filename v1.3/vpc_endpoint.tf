a8df8679dc79087d1cc41458a83ef8788f093fed
dennis.litvak <dennis.litvak@kaltura.com>
2022-01-20 15:27:43 +0200

    Changed Lambda to go through vpc-endpoint

--- v1.3/vpc_endpoint.tf
+++ v1.3/vpc_endpoint.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "lambda_send_url" {
   vpc_id                  = var.vpc_id
   service_name            = var.lightlytics_endpoint_service_name
-  subnet_ids              = values[var.endpoint_subnet_ids]
+  subnet_ids              = [var.endpoint_subnet_ids]
   private_dns_enabled     = true
 }
