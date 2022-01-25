be308461415a0b28e8a73884a6140ac3e87043ff
dennis.litvak <dennis.litvak@kaltura.com>
2022-01-25 13:04:02 +0200

    endpoint

--- v1.3/vpc_endpoint.tf
+++ v1.3/vpc_endpoint.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_endpoint" "lambda_send_url" {
   vpc_id                  = var.vpc_id
   service_name            = var.lightlytics_endpoint_service_name
-  subnet_ids              = values(var.endpoint_subnet_ids)
+  subnet_ids              = var.endpoint_subnet_ids
   private_dns_enabled     = true
   vpc_endpoint_type = "Interface"
   security_group_ids = [
